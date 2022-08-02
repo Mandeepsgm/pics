3a10723226e9363298192e8dca184a681c0032cb
Mandeepsgm <mandeepsg.m@gmail.com>
2022-08-02 03:43:16 -0400

    complete search pic app

---
+++ src/components/ImageList.js
@@ -0,0 +1,17 @@
+import './ImageList.css';
+import React from "react";
+import ImageCard from './ImageCard';
+
+const ImageList= (props)=>{
+    const images= props.images.map((image)=> {
+        return <ImageCard image={image} />
+    })
+    return (
+        <div className='image-list'>
+           {images}        
+        </div>
+                
+    );
+};
+
+export default ImageList;
